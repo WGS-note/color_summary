44cc157a4da64824b1a03a26a4dacf85ffccc5ad
my_git <1151573613@qq.com>
2024-03-27 13:40:04 +0800

    update

---
+++ setup.py
@@ -0,0 +1,44 @@
+import setuptools, os
+
+PACKAGE_NAME = 'wgs_color_summary'
+VERSION = '1.0'
+AUTHOR = 'wgs'
+EMAIL = '1151573613@qq.com'
+DESCRIPTION = '图片色系的提取与概述'
+GITHUB_URL = 'https://github.com/WGS-note/color_summary'
+
+parent_dir = os.path.dirname(os.path.realpath(__file__))
+import_name = os.path.basename(parent_dir)
+
+with open('{}/README.md'.format(parent_dir), 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=GITHUB_URL,
+    packages=[
+        'color_summary',
+        'color_summary.assets',
+        'color_summary.color_extract',
+    ],
+    package_dir={'color_summary':'.'},
+    package_data={'color_summary.assets': ['colors.csv', 'd7-1.jpg', 'webcolors.png']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'requests',
+        'scipy',
+        'opencv-python>=4.7.0.72',
+    ],
+)
